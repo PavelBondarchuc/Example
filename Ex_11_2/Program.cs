9e4ecf4ed5be43cfeaef948efaa6f132f853b46a
Pavel <akella1974@rambler.ru>
2022-05-26 16:26:17 +0300

    пример с масивом 3

---
+++ Ex_11_2/Program.cs
@@ -0,0 +1,57 @@
+﻿void FiLLArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index ++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+    }
+}
+
+int IndexoOf(int[] collection, int find)
+{
+    int count =collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        
+    
+
+
+        index ++;
+    }   
+    return position;
+}
+
+    
+    
+
+
+int[] array = new int[10];
+
+FiLLArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexoOf(array, 4);
+Console.WriteLine(pos);
